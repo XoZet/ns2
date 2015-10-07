71c41b568cfc5d2ade619a74153cad048f30c800
Absurdon <Absurdon@users.noreply.github.com>
2015-10-07 20:01:55 +0200

    some translation + csrf protection for login + start for registration functionality

--- src/AppBundle/Controller/User/RegistrationController.php
+++ src/AppBundle/Controller/User/RegistrationController.php
@@ -2,6 +2,7 @@
 namespace AppBundle\Controller\User;
 
 use AppBundle\Controller\User\UserBaseController as BaseController;
+use AppBundle\Form\Type\RegistrationFormType as RegistrationFormType;
 
 /**
 * 
@@ -11,7 +12,36 @@ class RegistrationController extends BaseController
 	
 	public function registerAction()
 	{
-		
+		$form = $this->createForm(new RegistrationFormType());
+        $formHandler = $this->container->get('fos_user.registration.form.handler');
+
+        $process = $formHandler->process($confirmationEnabled);
+        if ($process) {
+            $user = $form->getData();
+
+            $authUser = false;
+            if ($confirmationEnabled) {
+                $this->container->get('session')->set('fos_user_send_confirmation_email/email', $user->getEmail());
+                $route = 'fos_user_registration_check_email';
+            } else {
+                $authUser = true;
+                $route = 'fos_user_registration_confirmed';
+            }
+
+            $this->setFlash('fos_user_success', 'registration.flash.user_created');
+            $url = $this->container->get('router')->generate($route);
+            $response = new RedirectResponse($url);
+
+            if ($authUser) {
+                $this->authenticateUser($user, $response);
+            }
+
+            return $response;
+        }
+
+        return $this->container->get('templating')->renderResponse('FOSUserBundle:Registration:register.html.'.$this->getEngine(), array(
+            'form' => $form->createView(),
+        ));
 	}
 
 	public function checkMailAction()
---
+++ src/AppBundle/Form/Type/RegistrationFormType.php
@@ -0,0 +1,40 @@
+<?php
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+* 
+*/
+class RegistrationFormType extends AbstractType
+{
+	
+	public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', 'text', array('label' => 'form.username', 'translation_domain' => 'User'))
+            ->add('email', 'email', array('label' => 'form.email', 'translation_domain' => 'User'))
+            ->add('plainPassword', 'repeated', array(
+                'type' => 'password',
+                'options' => array('translation_domain' => 'User'),
+                'first_options' => array('label' => 'form.password'),
+                'second_options' => array('label' => 'form.password_confirmation'),
+                'invalid_message' => 'user.password.mismatch',
+            ))
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Security\User'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'user_registration';
+    }
+}
